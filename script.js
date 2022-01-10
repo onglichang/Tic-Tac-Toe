a0dc19925f13089d4a0cf02dfdcfac4b07e31d58
onglichang <onglichang@hotmail.com>
2022-01-10 14:11:04 +0800

    Add basic game board

--- script.js
+++ script.js
@@ -1,6 +1,25 @@
+//Set up gameboard
+function setUpBase() {
+    const boardDiv = document.getElementById("board-container");
+    for (let i=0; i < 3; i++) {
+        let horDiv = document.createElement('div');
+        horDiv.className = "hor-div";
+        for (let j=0; j < 3; j++) {
+            let verDiv = document.createElement('div');
+            verDiv.className = "unit"
+            horDiv.appendChild(verDiv);
+        }
+        boardDiv.appendChild(horDiv);
+    }
+}
+
+setUpBase();
+
 //Gameboard object - module
 const gameBoard = (() => {
-    const board = [];
+    const board = [[null, null, null],
+                    [null, null, null],
+                    [null, null, null]];
 })();
 
 //Display controller - module
