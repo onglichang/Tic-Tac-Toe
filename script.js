d32e5fb1af0b903a743617cc9f64697e454c8088
onglichang <onglichang@hotmail.com>
2022-01-10 11:14:22 +0800

    Add basic code files

---
+++ script.js
@@ -0,0 +1,15 @@
+//Gameboard object - module
+const gameBoard = (() => {
+    const board = [];
+})();
+
+//Display controller - module
+const displayController = (() => {
+
+})();
+
+//Players - factories 
+const Player = (marker) => {
+    return {marker};
+}
+
