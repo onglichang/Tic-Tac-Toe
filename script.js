e7dd0aca9b2c2d157eccb74b840021e2894cd0e1
onglichang <onglichang@hotmail.com>
2022-01-11 16:01:05 +0800

    Clean displayController code

--- script.js
+++ script.js
@@ -1,20 +1,3 @@
-//Set up gameboard
-function setUpBase() {
-    const boardDiv = document.getElementById("board-container");
-    for (let i=0; i < 3; i++) {
-        let horDiv = document.createElement('div');
-        horDiv.className = "hor-div";
-        for (let j=0; j < 3; j++) {
-            let verDiv = document.createElement('div');
-            verDiv.className = "unit"
-            horDiv.appendChild(verDiv);
-        }
-        boardDiv.appendChild(horDiv);
-    }
-}
-
-setUpBase();
-
 //Gameboard object - module
 const gameBoard = (() => {
     const board = [[null, null, null],
@@ -22,13 +5,46 @@ const gameBoard = (() => {
                     [null, null, null]];
 })();
 
-//Display controller - module
-const displayController = (() => {
-
-})();
-
 //Players - factories 
 const Player = (marker) => {
     return {marker};
 }
+
+let player1 = Player('X');
+let player2 = Player('O');
+
+//Display controller - module
+const displayController = (() => {
+    const setUpGame = () => {
+        // Set Up Game Board
+        const boardDiv = document.getElementById("board-container");
+        for (let i=0; i < 3; i++) {
+            let horDiv = document.createElement('div');
+            horDiv.className = "hor-div";
+            for (let j=0; j < 3; j++) {
+                let verDiv = document.createElement('div');
+                verDiv.className = "unit"
+                horDiv.appendChild(verDiv);
+            }
+            boardDiv.appendChild(horDiv);
+        }
+        setTurn(player1);
+    }
+
+    let currentPlayer;
+
+    const setTurn = (player) => {
+        currentPlayer = player;
+        const currentPlayerStatement = document.getElementById("current-player");
+        currentPlayerStatement.innerHTML = `Player ${player.marker}'s turn`;
+    }
+
+    return {
+        setUpGame,
+        setTurn
+    };
+})();
+
+displayController.setUpGame();
+
 
