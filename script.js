205333678cc08fe190dfee1811b58d4a2d49c816
onglichang <onglichang@hotmail.com>
2022-01-11 16:59:37 +0800

    Add basic marker placement functions

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ const gameBoard = (() => {
     const board = [[null, null, null],
                     [null, null, null],
                     [null, null, null]];
+    return {board};
 })();
 
 //Players - factories 
@@ -10,11 +11,12 @@ const Player = (marker) => {
     return {marker};
 }
 
-let player1 = Player('X');
-let player2 = Player('O');
-
 //Display controller - module
 const displayController = (() => {
+    let firstPlayer = Player('X');
+    let secondPlayer = Player('O');
+    let currentPlayer;
+
     const setUpGame = () => {
         // Set Up Game Board
         const boardDiv = document.getElementById("board-container");
@@ -23,25 +25,42 @@ const displayController = (() => {
             horDiv.className = "hor-div";
             for (let j=0; j < 3; j++) {
                 let verDiv = document.createElement('div');
-                verDiv.className = "unit"
+                verDiv.className = "unit";
+                verDiv.setAttribute("position", `${i}${j}`);
+                verDiv.addEventListener('click', addMarker);
                 horDiv.appendChild(verDiv);
             }
             boardDiv.appendChild(horDiv);
         }
-        setTurn(player1);
+        setTurn(firstPlayer);
     }
 
-    let currentPlayer;
-
+    
     const setTurn = (player) => {
         currentPlayer = player;
         const currentPlayerStatement = document.getElementById("current-player");
         currentPlayerStatement.innerHTML = `Player ${player.marker}'s turn`;
     }
 
+    const addMarker = (e) => {
+        if (!!e.target.innerHTML) {
+            console.log('something here already');
+        } else {
+            const position = e.target.getAttribute("position");
+            const row = position.charAt(0);
+            const col = position.charAt(1);
+            gameBoard.board[row][col] = currentPlayer.marker;
+            e.target.innerHTML = currentPlayer.marker;
+            // Test Statement
+            console.log(gameBoard.board);
+        }
+        setTurn(secondPlayer)
+        secondPlayer = firstPlayer;
+        firstPlayer = currentPlayer;
+    } 
+
     return {
-        setUpGame,
-        setTurn
+        setUpGame
     };
 })();
 
