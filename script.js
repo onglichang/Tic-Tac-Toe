cb0fa01a78cff84340ab24610495630d468f82f4
onglichang <onglichang@hotmail.com>
2022-01-11 17:33:54 +0800

    Add check board winner functionality

--- script.js
+++ script.js
@@ -3,7 +3,32 @@ const gameBoard = (() => {
     const board = [[null, null, null],
                     [null, null, null],
                     [null, null, null]];
-    return {board};
+
+    const checkBoard = (marker) => {
+        const tBoard = board[0].map((_, colIndex) => board.map(row => row[colIndex]));
+        const leftDiagonal = [board[0][0], board[1][1], board[2][2]];
+        const rightDiagonal = [board[0][2], board[1][1], board[2][0]];
+        //Check diagonal
+        if (leftDiagonal.every((val) => val === leftDiagonal[0] && val != null)) {
+            alert(`Player ${marker} won!`);
+        } else if (rightDiagonal.every((val) => val === rightDiagonal[0] && val != null)) {
+            alert(`Player ${marker} won!`);
+        } else {
+            for (let i = 0; i < 3; i++) {
+                const row = board[i];
+                const tRow = tBoard[i];
+                //Check horizontal 
+                if (row.every((val) => val === row[0] && val != null)) {
+                    alert(`Player ${marker} won!`);
+                //Check vertical
+                } else if (tRow.every((val) => val === tRow[0] && val != null)) {
+                    alert(`Player ${marker} won!`);
+                } 
+            }
+        }
+
+    }
+    return {board, checkBoard};
 })();
 
 //Players - factories 
@@ -44,19 +69,22 @@ const displayController = (() => {
 
     const addMarker = (e) => {
         if (!!e.target.innerHTML) {
-            console.log('something here already');
+            alert('Position already taken, please try again');
         } else {
             const position = e.target.getAttribute("position");
             const row = position.charAt(0);
             const col = position.charAt(1);
             gameBoard.board[row][col] = currentPlayer.marker;
             e.target.innerHTML = currentPlayer.marker;
+            gameBoard.checkBoard(currentPlayer.marker);
             // Test Statement
-            console.log(gameBoard.board);
+            //console.log(gameBoard.board);
+
+            setTurn(secondPlayer)
+            secondPlayer = firstPlayer;
+            firstPlayer = currentPlayer;
         }
-        setTurn(secondPlayer)
-        secondPlayer = firstPlayer;
-        firstPlayer = currentPlayer;
+        
     } 
 
     return {
